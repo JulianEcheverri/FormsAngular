47f1aeaae5678a4eeb3993248c30ad3b2019e33d
JulianEcheverri <julianecheverri@outlook.com>
2019-12-02 15:14:02 -0500

    reactive forms

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -13,12 +13,41 @@ export class DataComponent implements OnInit {
   // variable para manejar el formulario
   formulario: FormGroup;
 
+  usuario: any = {
+    nombrecompleto: {
+      nombre: 'Julian',
+      apellido: 'Echeverri'
+    },
+    correo: 'julianecheverri@outlook.com'
+  }
+
+  // para asociarlo en el template se hace con la relacion de la propiedad [formGroup] = "formulario"
   constructor() { 
     // el objeto de FormGroup necesita una estructra para ser inicializado
-    this.formulario = new FormGroup();
+    this.formulario = new FormGroup({
+      'nombrecompleto': new FormGroup({
+        'nombre' : new FormControl('', [Validators.required, Validators.minLength(3)]), 
+        'apellido' : new FormControl('', Validators.required), 
+      }),
+      //pasandole un valor por defecto, reglas de validacion, y reglas de validacion asincrona
+      // 'nombre' : new FormControl('Julian'), 
+      // con validators
+      // 'nombre' : new FormControl('', [Validators.required, Validators.minLength(3)]), 
+      // 'apellido' : new FormControl('', Validators.required), 
+      'correo' : new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]), 
+    });
+
+
   }
 
   ngOnInit() {
   }
+
+  guardarCambios(){
+    console.log(this.formulario.value);
+    console.log(this.formulario);
+    console.log(this.usuario);
+  }
+
 
 }
