7100cae007fba00857f5fad0ec4564938b5a2ba1
JulianEcheverri <julianecheverri@outlook.com>
2019-12-03 13:04:34 -0500

    More forms

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // trabajando con formularios reactivos
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
 // se debe agregar ReactiveFormsModule en el app.module
 
 @Component({
@@ -18,7 +18,8 @@ export class DataComponent implements OnInit {
       nombre: 'Julian',
       apellido: 'Echeverri'
     },
-    correo: 'julianecheverri@outlook.com'
+    correo: 'julianecheverri@outlook.com',
+    //pasatiempos: ['Correr', 'Dormir', 'Comer']
   }
 
   // para asociarlo en el template se hace con la relacion de la propiedad [formGroup] = "formulario"
@@ -29,13 +30,23 @@ export class DataComponent implements OnInit {
         'nombre' : new FormControl('', [Validators.required, Validators.minLength(3)]), 
         'apellido' : new FormControl('', Validators.required), 
       }),
+
+
       //pasandole un valor por defecto, reglas de validacion, y reglas de validacion asincrona
       // 'nombre' : new FormControl('Julian'), 
       // con validators
       // 'nombre' : new FormControl('', [Validators.required, Validators.minLength(3)]), 
       // 'apellido' : new FormControl('', Validators.required), 
       'correo' : new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]), 
+      // para una array
+      'pasatiempos' : new FormArray([
+        new FormControl('Correr', Validators.required )
+      ])
     });
+
+    // inicializar valores 
+    //this.formulario.setValue(this.usuario);
+
 
 
   }
@@ -47,6 +58,19 @@ export class DataComponent implements OnInit {
     console.log(this.formulario.value);
     console.log(this.formulario);
     console.log(this.usuario);
+    this.formulario.reset({
+      nombrecompleto: {
+        nombre: '',
+        apellido: ''
+      },
+      correo: ''
+    });
+  }
+
+  agregarPasatiempo(){
+    (<FormArray>this.formulario.controls['pasatiempos']).push(
+      new FormControl('', Validators.required)
+    )
   }
 
 
